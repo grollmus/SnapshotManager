63c8e220ee8956160bdfbddbf7d8737cc4418d23
Christian Neukirchinger <christian.neukirchinger@gmail.com>
2023-06-14 11:49:33 +0200

    moving SnapshotManager restore logic to own class, removing unnecessary "using" directives

--- SnapshotManagerContextMenuAddIn.cs
+++ SnapshotManagerContextMenuAddIn.cs
@@ -1,18 +1,7 @@
-﻿using System.Windows.Forms;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿
 using Siemens.Engineering;
 using Siemens.Engineering.AddIn.Menu;
 using Siemens.Engineering.SW.Blocks;
-using Siemens.Engineering.SW.Blocks.Interface;
-using Siemens.Engineering.SW;
-using Siemens.Engineering.HW;
-using Siemens.Engineering.HW.Features;
-using System.Diagnostics;
-using System.Xml.Linq;
-using System.Xml;
-using SnapshotManager;
 
 namespace SnapshotManager
 {
@@ -113,33 +102,8 @@ namespace SnapshotManager
         /// </param>
         private void OnDoRestoreSnapshot(MenuSelectionProvider<GlobalDB> menuSelectionProvider)
         {
-            // TODO: Change the code here
-            // Program of AddIn
-            foreach(GlobalDB curSelection in menuSelectionProvider.GetSelection<GlobalDB>()) {
-                XmlDocument snapShotXML = new XmlDocument();
-                snapShotXML.Load("C:\\temp\\MyInterfaceSnapshot.xml");
-                XmlNode root = snapShotXML.DocumentElement;
-                XmlNamespaceManager nsmgr = new XmlNamespaceManager(snapShotXML.NameTable);
-                nsmgr.AddNamespace("sv", "http://www.siemens.com/automation/Openness/SW/Interface/Snapshot/v1");
-
-                XmlNodeList savedXmlValues = root.SelectNodes("descendant::sv:Value[@Path]", nsmgr);
-                foreach(XmlNode savedXmlvalue in savedXmlValues)
-                {
-                    Debug.WriteLine(savedXmlvalue.InnerText);
-                    XmlAttributeCollection attributes = savedXmlvalue.Attributes;
-                    foreach(XmlAttribute attribute in attributes)
-                    {
-                        if(attribute.Name == "Path") 
-                        {
-                            PlcBlockInterface plcBlockInterface = curSelection.Interface;
-                            MemberComposition members = plcBlockInterface.Members;
-                            Member member = members.Find(attribute.InnerText);   
-                            member.SetAttribute("StartValue", savedXmlvalue.InnerText);
-                        }
-                        
-                    }
-                }                
-            }
+            SnapshotManager snapshotManager = new SnapshotManager();
+            snapshotManager.restoreSnapshot(menuSelectionProvider);
         }
 
         /// <summary>
--- SnapshotManger.cs
+++ SnapshotManger.cs
@@ -3,6 +3,8 @@ using Siemens.Engineering.AddIn.Menu;
 using Siemens.Engineering.SW.Blocks;
 using Siemens.Engineering.SW.Blocks.Interface;
 using System.IO;
+using System.Xml;
+using System.Diagnostics;
 
 namespace SnapshotManager
 {
@@ -21,9 +23,33 @@ namespace SnapshotManager
             }
         }
 
-        public void restoreSnapshot()
+        public void restoreSnapshot(MenuSelectionProvider<GlobalDB> menuSelectionProvider)
         {
+            foreach(GlobalDB curSelection in menuSelectionProvider.GetSelection<GlobalDB>()) {
+                XmlDocument snapShotXML = new XmlDocument();
+                snapShotXML.Load("C:\\temp\\MyInterfaceSnapshot.xml");
+                XmlNode root = snapShotXML.DocumentElement;
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(snapShotXML.NameTable);
+                nsmgr.AddNamespace("sv", "http://www.siemens.com/automation/Openness/SW/Interface/Snapshot/v1");
 
+                XmlNodeList savedXmlValues = root.SelectNodes("descendant::sv:Value[@Path]", nsmgr);
+                foreach(XmlNode savedXmlvalue in savedXmlValues)
+                {
+                    Debug.WriteLine(savedXmlvalue.InnerText);
+                    XmlAttributeCollection attributes = savedXmlvalue.Attributes;
+                    foreach(XmlAttribute attribute in attributes)
+                    {
+                        if(attribute.Name == "Path") 
+                        {
+                            PlcBlockInterface plcBlockInterface = curSelection.Interface;
+                            MemberComposition members = plcBlockInterface.Members;
+                            Member member = members.Find(attribute.InnerText);   
+                            member.SetAttribute("StartValue", savedXmlvalue.InnerText);
+                        }
+                        
+                    }
+                }                
+            }
         }
     }
 }
